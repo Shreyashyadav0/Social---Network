9393903eda48e34b237afbaf9874228e9b870ef9
Shreyashyadav0 <shreyashyadav0@gmail.com>
2023-01-28 12:27:12 +0530

    all done and dusted

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ There three things to learn through this project
 logging - we make the log of the user to see what is going right and what is wrong sometimes user may be some old version or it may be on 2g or something so he may not be able to login so we make the log using "MORGAN " we console log it and then 
 we fix it in out next version.
  there are services like logdog to give you ui of all the data 
+Create a .env file in config folder and add the following as key = value
+PORT = 8000 (can be any port example: 3000)
+DB_STRING = your database URI
+CLOUD_NAME = your cloudinary cloud name
+API_KEY = your cloudinary api key
+API_SECRET = your cloudinary api secret
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,49 @@
-const passport = require("passport");
-const validator = require("validator");
-const User = require("../models/User");
+// const passport = require("passport");
+// const validator = require("validator");
+// const User = require("../models/User");
 
-exports.login= (req, res, next) =>{
-    const validationErroes = [];
-    if(!validator.isEmail(req.body.email)){
-        validationErroes.push({ msg: "Enter valid Email"});
-    }
-    if ( validator.isEmpty(req.body.password)) {
-        validationErroes.push({ msg: "Enter valid Password"});
-    }
-    if (validationErroes.length >0) {
-        return res.redirect("/login");
-    }
+// exports.login= (req, res, next) =>{
+//     const validationErroes = [];
+//     if(!validator.isEmail(req.body.email)){
+//         validationErroes.push({ msg: "Enter valid Email"});
+//     }
+//     if ( validator.isEmpty(req.body.password)) {
+//         validationErroes.push({ msg: "Enter valid Password"});
+//     }
+//     if (validationErroes.length >0) {
+//         return res.redirect("/login");
+//     }
 
-    req.body.email = validator.normalizeEmail(req.body.email, {
-        gmail_remove_dots: true
-    });
+//     req.body.email = validator.normalizeEmail(req.body.email, {
+//         gmail_remove_dots: true
+//     });
 
-    passport.authenticate("local", (err, user, info) =>{
-        if(err) {
-            return next(err)
-        }
-        if(!user){
-            return res.redirect("/login");
-        }
+//     passport.authenticate("local", (err, user, info) =>{
+//         if(err) {
+//             return next(err)
+//         }
+//         if(!user){
+//             return res.redirect("/login");
+//         }
 
-        req.lo
-    })
-    }
+//         req.lo
+//     })
+//     }
+
+module.exports = {
+    ensureAuth: function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      } else {
+        res.redirect("/");
+      }
+    },
+    ensureGuest: function (req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      } else {
+        res.redirect("/dashboard");
+      }
+    },
+  };
+  
